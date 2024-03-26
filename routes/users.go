7377269134e0bd09c93af2b9a891b790246250d8
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-03-25 20:07:02 -0400

    finished JWT logic and token verification utility

--- routes/users.go
+++ routes/users.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"event-booking-api/models"
+	"event-booking-api/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -42,5 +43,10 @@ func login(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user."})
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
 }
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,15 +1,52 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func GenerateToken() {
-	jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email":  "",
-		"userID": "",
+const secretKey = "supersecret" // dummy key
+
+func GenerateToken(email string, userID int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userID": userID,
 		"exp":    time.Now().Add(time.Hour * 2).Unix(),
 	})
+
+	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return errors.New("could not parse token")
+	}
+
+	if !(parsedToken.Valid) {
+		return errors.New("invalid token")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	_, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return errors.New("invalid token claims")
+	}
+
+	// email := claims["email"].(string)
+	// userID := claims["userID"].(int64)
+
+	return nil
 }
